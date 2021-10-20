777b2274c4040b3aa6289062d6d5343e96eb0245
gudtjr3437 <gudtjr3437@naver.com>
2021-10-20 20:07:40 +0900

    feat : 글 생성 기능

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ const dataList = [
 const search_input = document.querySelector("#search_input");
 const search_bnt = document.querySelector("#search_btn");
 
+const create_form = document.querySelector("#create_form");
 const create_btn = document.querySelector("#create_btn");
-const create_input = document.querySelector("#create_input");
+
+//화면에 데이터 뿌려줌
+const 
+
+
 
 //검색
 const serachData = () => {
@@ -35,10 +40,45 @@ const serachData = () => {
 };
 
 //생성
-const createData = () => {};
+const showCreateForm = () => {
+  //보이도록
+  create_form.classList.remove("vis_none");
+  const create_submit_btn = document.querySelector("#create_submit_btn");
 
-//이벤트 adapter
+  create_submit_btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    createData();
+  });
+};
+
+const createData = () => {
+  //상단 선언부에서 선언시 오류남 (display="none")때문
+  const create_title_input = document.querySelector("#create_title_input");
+  const create_body_input = document.querySelector("#create_body_input");
+  const curIdx = dataList[dataList.length - 1].id;
+  console.log(curIdx);
+
+  dataList.push({
+    id: curIdx + 1,
+    title: create_title_input.value,
+    body: create_body_input.value,
+  });
+  console.log(dataList);
+  //폼 다시 랜더링 되도록 함수 호출
+};
+
+
+/*
+이벤트 adapter
+*/
+
+//검색 버튼
 search_btn.addEventListener("click", (event) => {
   event.preventDefault();
   serachData();
+});
+
+//생성 버튼
+create_btn.addEventListener("click", () => {
+  showCreateForm();
 });
