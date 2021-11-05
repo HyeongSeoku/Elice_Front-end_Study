2a8c58a47653ea0c109fb6889e757863a84b5e78
gudtjr3437 <gudtjr3437@naver.com>
2021-11-05 19:06:33 +0900

    Feat : modal 추가

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import styled from "styled-components";
 import PostForm from "../components/PostForm";
 import PostData from "../components/PostData";
+import PostModal from "../components/PostModal";
 
 const FeedContainer = styled.div`
     padding : 20px;
@@ -29,7 +30,7 @@ const PostMain = styled.main`
     align-items: center;
 `
 
-const Feed = ({data}) => {
+const Feed = ({data,modal}) => {
 
     return (
         <FeedContainer>    
@@ -39,19 +40,21 @@ const Feed = ({data}) => {
                 <FeedContents>
                     <PostList>
                         {data.map((post)=>(
-                            <PostData key={post.id} {...post}/>
+                            <PostData key={post.id} {...post} modal={modal}/>
                         ))}
                     </PostList>
                 </FeedContents>
             </PostMain>
+            <PostModal visibile={modal}/>
         </FeedContainer>
     )
 }
 
 //connect함수에 첫번째 인수로 들어가는 함수 or 객체
 //기본적으로 store가 업데이트가 될때마다 자동적으로 호출이 됨
-const mapStateToProps = (state) =>{
-    return {data:state.postData};
+const mapStateToProps = (state,ownProps) =>{
+    return {data:state.postData,modal:state.modal.isOpen};
+    
 }
 
 //connect 함수는 Provider 컴포넌트 하위에 존재하는 컴포넌트들이 Store에 접근하게 만드는 역할
