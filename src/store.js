e413390ec5481fd0f281554e24f897d8df070d85
gudtjr3437 <gudtjr3437@naver.com>
2021-11-05 19:05:00 +0900

    Feat : modal reducer 추가

--- src/store.js
+++ src/store.js
@@ -1,15 +1,24 @@
 import { combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import post_reducer from "./reducer/post";
+import modal_reducer from "./reducer/modal";
 import * as postAction from "./action/postAction";
+import * as modalAction from "./action/modalAction";
 
 //post Action
 const post_createData = postAction.post_createData;
 const post_deleteData = postAction.post_deleteData;
 const post_updateData = postAction.post_updateData;
 
+//modal Action
+const modal_open = modalAction.modal_open;
+const modal_close = modalAction.modal_close;
+
 //분리된 리듀서 연결
-const reducer = combineReducers({ postData: post_reducer });
+const reducer = combineReducers({
+  postData: post_reducer,
+  modal: modal_reducer,
+});
 
 //composeWithDevTools() : 크롬에서 Redux extension 사용하기 위한 세팅
 const store = createStore(reducer, composeWithDevTools());
@@ -19,6 +28,8 @@ export const actionCreators = {
   post_createData,
   post_deleteData,
   post_updateData,
+  modal_open,
+  modal_close,
 };
 
 export default store;
