894c01ca5e214ff8f984684e6471b5ce5fd12a41
gudtjr3437 <gudtjr3437@naver.com>
2021-11-12 03:49:48 +0900

    Feat : Api 동작 테스트 (✅)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import Header from "../components/Header";
 import { actionCreators } from "../store";
 import styled from "styled-components";
+import api from "../apis/giphyApi";
 
 
 const Home = () => {
+  useEffect(()=>{
+    const fetchData = async () =>{
+      const data = await api.randomGif();
+    console.log(data);
+    }
+    fetchData();
+  },[])
   
   return (
     <>
