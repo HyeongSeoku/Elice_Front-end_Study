202ac891d4de8cbfc6e762ba2054d507a4ef5ab1
gudtjr3437 <gudtjr3437@naver.com>
2021-11-12 01:41:23 +0900

    Refactor : Post Reducer refactoring with createReducer

--- src/reducer/post.js
+++ src/reducer/post.js
@@ -1,4 +1,10 @@
-import { createPost, deletePost, updatePost } from "../action/postAction";
+import { createAction } from "@reduxjs/toolkit";
+import { createReducer } from "@reduxjs/toolkit";
+
+//action
+const createPost = createAction("CREATE_POST");
+const deletePost = createAction("DELETE_POST");
+const updatePost = createAction("UPDATE_POST");
 
 const initialState = [
   {
@@ -15,29 +21,46 @@ const initialState = [
   },
 ];
 
-const post_reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case createPost.type:
-      return [
-        ...state,
-        {
-          id: parseInt(Date.now()) + Math.floor(Math.random() * 10),
-          title: action.payload.title,
-          body: action.payload.body,
-          img_url: "",
-        },
-      ];
-    case deletePost.type:
-      return state.filter((data) => data.id !== action.payload);
-    //수정 액션 (! complete)
-    case updatePost.type:
-      return [
-        ...state,
-        { id: action.id, title: action.title, body: action.body },
-      ];
-    default:
-      return state;
-  }
-};
+//immer
+//createReducer = 할 수 있는 두가지 옵션 (return || mutate)
 
-export default post_reducer;
+const post_reducer = createReducer(initialState, {
+  [createPost]: (state, action) => {
+    state.push({
+      id: parseInt(Date.now()) + Math.floor(Math.random() * 10),
+      title: action.payload.title,
+      body: action.payload.body,
+      img_url: "",
+    });
+  },
+  [deletePost]: (state, action) =>
+    state.filter((data) => data.id !== action.payload),
+  // [updatePost]:(state,action) =>
+});
+
+// const post_reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case createPost.type:
+//       return [
+//         ...state,
+//         {
+//           id: parseInt(Date.now()) + Math.floor(Math.random() * 10),
+//           title: action.payload.title,
+//           body: action.payload.body,
+//           img_url: "",
+//         },
+//       ];
+//     case deletePost.type:
+//       return state.filter((data) => data.id !== action.payload);
+//     //수정 액션 (! complete)
+//     case updatePost.type:
+//       return [
+//         ...state,
+//         { id: action.id, title: action.title, body: action.body },
+//       ];
+//     default:
+//       return state;
+//   }
+// };
+
+export { post_reducer, createPost, deletePost, updatePost };
