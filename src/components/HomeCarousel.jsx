d28a03b2b4714884b0306c0241c7f449e7ec6412
gudtjr3437 <gudtjr3437@naver.com>
2021-11-12 14:50:46 +0900

    Feat : api에서 받아온 데이터 랜더링

---
+++ src/components/HomeCarousel.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const ItemContainer = styled.div`
+    margin-right: 5px;
+`
+const ItemImg = styled.img.attrs(({url})=>({
+    src: url || "",
+}))`
+    width: ${props=>props.width};
+    height: ${props=>props.height};
+`
+
+const HomeCarousel = (props) =>{
+
+    return(
+        <ItemContainer>
+            <ItemImg url={props.url} width={props.width} heigth={props.height}/>
+        </ItemContainer>
+    )
+}
+
+export default HomeCarousel
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,24 +4,70 @@ import Header from "../components/Header";
 import { actionCreators } from "../store";
 import styled from "styled-components";
 import api from "../apis/giphyApi";
+import HomeCarousel from "../components/HomeCarousel";
 
+const ContentsMain = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+`;
+
+const HomeMessage = styled.h1`
+  color: white;
+`;
+
+const CarouselContainer = styled.div`
+  display: grid;
+  width: 1500px;
+  height: 500px;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  overflow: hidden; //공간을 넘어가는 이미지는 보이지 않도록 처리
+  &:hover {
+    transition: all 0.5s ease-in-out; //부드럽게 움직이도록
+    transform: translateX(-30%);
+  }
+`;
 
 const Home = () => {
-  useEffect(()=>{
-    const fetchData = async () =>{
-      const data = await api.randomGif();
-    console.log(data);
-    }
+  const [randomImg, setRandomImg] = useState([]); //홈 화면에 띄우는 용도로만 쓰기 때문에 useState로 선언 (not Redux)
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await api.randomGif();
+      const { data } = response;
+      const tempArr = [];
+      data.map((item) => {
+        tempArr.push(item);
+      });
+      //setRandomImg(tempArr);
+      setRandomImg(tempArr);
+    };
     fetchData();
-  },[])
-  
+  }, []);
+
+  //상태 변경 감지
+  useEffect(() => {
+    const stateCheck = async () => {
+      await console.log(randomImg);
+    };
+    stateCheck();
+  }, [randomImg]);
+
   return (
     <>
       <Header />
-      
+      <ContentsMain>
+        <HomeMessage>이미지를 둘러보고 공유해보세요.</HomeMessage>
+        <CarouselContainer>
+          {randomImg.map((item) => (
+            <HomeCarousel key={item.id} url={item.images.fixed_height.url} />
+          ))}
+        </CarouselContainer>
+      </ContentsMain>
     </>
   );
 };
-
 
 export default Home;
