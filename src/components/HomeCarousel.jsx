6651e9672b794aebcf77af9c9d8fab3acc443a16
gudtjr3437 <gudtjr3437@naver.com>
2021-11-12 16:14:11 +0900

    Feat : 슬라이드를 위한 이미지 컴포넌트

--- src/components/HomeCarousel.jsx
+++ src/components/HomeCarousel.jsx
@@ -3,22 +3,21 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const ItemContainer = styled.div`
-    margin-right: 5px;
-`
-const ItemImg = styled.img.attrs(({url})=>({
-    src: url || "",
+  margin-right: 5px;
+`;
+const ItemImg = styled.img.attrs(({ url }) => ({
+  src: url || "",
 }))`
-    width: ${props=>props.width};
-    height: ${props=>props.height};
-`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+`;
 
-const HomeCarousel = (props) =>{
+const HomeCarousel = (props) => {
+  return (
+    <ItemContainer>
+      <ItemImg url={props.url} width={props.width} heigth={props.height} />
+    </ItemContainer>
+  );
+};
 
-    return(
-        <ItemContainer>
-            <ItemImg url={props.url} width={props.width} heigth={props.height}/>
-        </ItemContainer>
-    )
-}
-
-export default HomeCarousel
+export default HomeCarousel;
--- src/pages/Surf.jsx
+++ src/pages/Surf.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import styled from "styled-components";
 
+const SurfContainer = styled.div`
+  padding: 20px;
+`;
+
+const SurfTitle = styled.h1`
+  color: white;
+`;
 
 const Surf = () => {
-
-    return (
-        <>
-            <h2>Surf</h2>
-            <div>surfing</div>
-        </>
-    )
-}
+  return (
+    <SurfContainer>
+      <SurfTitle>Surf</SurfTitle>
+      <div>surfing</div>
+    </SurfContainer>
+  );
+};
 
 export default Surf;
