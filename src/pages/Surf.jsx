021b3722b7c384dcd3ecaa53c7f82e89faff4f03
gudtjr3437 <gudtjr3437@naver.com>
2021-11-13 19:09:40 +0900

    Feat : 검색 페이지 수정

--- src/pages/Surf.jsx
+++ src/pages/Surf.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import SearchData from "../components/SearchData";
+import SearchForm from "../components/SearchForm";
 
 const SurfContainer = styled.div`
   padding: 20px;
@@ -9,11 +11,33 @@ const SurfTitle = styled.h1`
   color: white;
 `;
 
+const ResultContainer = styled.div`
+  width: auto;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+`;
+
 const Surf = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
     <SurfContainer>
       <SurfTitle>Surf</SurfTitle>
-      <div>surfing</div>
+      <SearchForm setData={setData} data={data} />
+      <ResultContainer>
+        {data.map((item) => (
+          <SearchData
+            key={item.id}
+            url={item.images.fixed_height.url}
+            title={item.title}
+          />
+        ))}
+      </ResultContainer>
     </SurfContainer>
   );
 };
